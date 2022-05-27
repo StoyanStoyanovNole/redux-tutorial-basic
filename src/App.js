244df06957c113273323b0f6f65101ea15082544
sstoyanov <stoyan.stoyanov.extern@seven.one>
2022-05-28 01:42:33 +0300

    init commit of redux-tutorial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from "./actions";
 
 function App() {
+  const counter = useSelector((state) => state.counter);
+  const isLogged = useSelector((state) => state.isLogged);
+  const dispatch = useDispatch();
+  /* Extract the counter for the store */
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello! Counter is {counter}</h1>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+      {isLogged ? <h3>Data I should not see!</h3> : ""}
+      {/* Use useSelector hook */}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { Provider } from "react-redux";
+import allReducers from "./reducers";
+import { configureStore } from "redux";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+// STORE --> GLOBALIZED STATE STORING ALL APP DATA
+const store = configureStore(
+  allReducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
+// ACTION --> DESCRIBES WHAT WE WANT TO DO WITH THE DATA i.e. increment, decrement etc. FUNCTION RETURNING AN OBJECT
+const increment = () => {
+  return {
+    type: "INCREMENT",
+  };
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const decrement = () => {
+  return {
+    type: "DECREMENT",
+  };
+};
+// REDUCER --> HOW THE ACTION MUTATES
+const counter = (state = 0, action) => {
+  // ADD A COUNTER -- how the initial state will look like
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+    default:
+      return;
+  }
+};
+
+/* let store = createStore(counter);
+// DISPATCH --> SEND ACTION (function with an object) TO THE REDUCER. REDUCER WILL TAKE A LOOK AT WHAT WAS DISPATCHED.
+store.dispatch(increment());
+store.dispatch(decrement());
+store.dispatch(decrement());
+ */
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
+);
